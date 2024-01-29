2941d0853f258f7efb15e8e7826eebed868ee5d7
Ryan <regriff@users.noreply.github.com>
2024-01-29 14:57:19 -0500

    added member map

---
+++ assets/js/map-data.js
@@ -0,0 +1,23 @@
+var membershipData = {
+  US: 69, // United States
+  GB: 11, // United Kingdom
+  NL: 7, // Netherlands
+  KR: 6, // South Korea
+  IT: 6, // Italy
+  DE: 5, // Germany
+  IE: 5, // Ireland
+  SE: 5, // Sweden
+  IN: 4, // India
+  CH: 3, // Switzerland
+  AU: 3, // Australia
+  CA: 3, // Canada
+  TZ: 2, // Tanzania
+  ES: 2, // Spain
+  AR: 1, // Argentina
+  BE: 1, // Belgium
+  BW: 1, // Botswana
+  HR: 1, // Croatia
+  FR: 1, // France
+  GR: 1, // Greece
+  IL: 1 // Israel
+};
